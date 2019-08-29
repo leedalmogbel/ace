54051e87e49c5b5c24b7a69657385b1550b00c2b
Benjamin Matias <bmatias@stratpoint.com>
2019-08-29 14:10:14 +0800

    Brewery Broilerplate added

---
+++ ace-node-backend/tennis-trainer/src/infra/dataSources/Tennis-trainerdb.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  name: 'tennis-trainerdb',
+  connector : 'sql',
+  config: {
+    host: '127.0.0.1',
+    username: 'root',
+    password: '',
+    database: 'tennis',
+    dialect: 'postgres',
+    isSync: 'true',
+
+  }
+};
