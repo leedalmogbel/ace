a257fd7548227e543ebcb77cc4bc09d7ae27f94a
Benjamin Matias <bmatias@stratpoint.com>
2019-08-29 14:19:12 +0800

    fix folder structure

---
+++ ace-node-backend/src/app.js
@@ -0,0 +1,13 @@
+const { brew } = require('@brewery/core');
+const config = require('config');
+
+const { server, container } = brew(config);
+
+server
+  .start()
+  .catch((error) => {
+    server.logger.error(error.stack);
+    process.exit();
+  });
+
+module.exports = container;
