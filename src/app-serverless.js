895c858e50cd5785ea25f707673f309001e6b31f
Kristine Boquiren <kboquiren@stratpoint.com>
2019-12-16 13:50:47 +0800

    Add service for caalling third part apis

--- src/app-serverless.js
+++ src/app-serverless.js
@@ -1,11 +1,17 @@
 require('module').Module._initPaths();
+const { Lifetime, asClass} = require('awilix');
 const { brew } = require('@amberjs/core');
 const config = require('config');
+const ThirdPartyApis = require('src/infra/services/ThirdPartyApis');
 
 module.exports.handler = (event, context, callback) => {
   brew(config, async (err, brewed) => {
     if (err) throw err;
-
+    brewed.container.register({
+      ThirdPartyApis: asClass(ThirdPartyApis, {
+        lifetime: Lifetime.SINGLETON
+      })
+    });
     const app = brewed.getServerless();
     try {
       const res = await app(event, context);
@@ -15,5 +21,3 @@ module.exports.handler = (event, context, callback) => {
     }
   });
 };
-
-
