2b7bd287be2e82fe2994ccb801b6c967d8a229fd
Kristine Boquiren <kboquiren@stratpoint.com>
2019-12-18 19:21:57 +0800

    Add clipId

--- src/infra/repositories/ClipPersonRepository.js
+++ src/infra/repositories/ClipPersonRepository.js
@@ -3,6 +3,7 @@ const { BaseRepository } = require('@amberjs/core');
 
 const reformatForKeypointsGeneration = clipPerson => {
   const newObj = {
+    clip_id: clipPerson.clipId,
     clip_person_id: clipPerson.id,
     centroid: clipPerson.centroid,
     start: clipPerson.clip.startTime,
@@ -56,6 +57,27 @@ class ClipPersonRepository extends BaseRepository {
   }
 
   async getDataForKeypointsGeneration(id) {
+    const clipPerson = await this._getById(id, {
+      attributes: ['id', 'centroid'],
+      include: [
+        {
+          model: this.ClipModel,
+          attributes: ['startTime', 'endTime'],
+          as: 'clip',
+          include: [
+            {
+              model: this.VideoModel,
+              attributes: ['path'],
+              as: 'video'
+            },
+          ]
+        },
+      ]
+    });
+    return reformatForKeypointsGeneration(clipPerson);
+  }
+
+  async getDataForVideoGeneration(id) {
     const clipPerson = await this._getById(id, {
       attributes: ['id', 'centroid'],
       include: [
