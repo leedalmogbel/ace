e72dc8cbffb846783bda80fc81c1822dbe690ebf
Kristine Boquiren <kboquiren@stratpoint.com>
2020-01-10 17:18:10 +0800

    Remove unnecessary code.

--- src/app/Clip/DetectedPerson/ShowDetectedPersonScore.js
+++ src/app/Clip/DetectedPerson/ShowDetectedPersonScore.js
@@ -3,9 +3,8 @@ const Utils = require('src/infra/services/utils');
 
 //select person
 class ShowDetectedPersonScore extends Operation {
-  constructor({ ClipPersonRepository, ThirdPartyApis, ScoreRepository }) {
+  constructor({ThirdPartyApis, ScoreRepository }) {
     super();
-    this.ClipPersonRepository = ClipPersonRepository;
     this.ScoreRepository = ScoreRepository;
     this.ThirdPartyApis = ThirdPartyApis;
   }
@@ -16,20 +15,13 @@ class ShowDetectedPersonScore extends Operation {
     } = this.events;
 
     try {
-    //   data.status = 'forKeypointsGeneration';
       const detectedPersonScores = await this.ScoreRepository.getAllWithParams({'testId': selectedPersonId});
       let message = 'Successfully generated.'; // update detectedPerson Scores , add field to detected person or create new table for score
-      // const scores = {
-      //   'scores': (0.3243244, 0.6756756)
-      // };
       if(detectedPersonScores){
         message = 'Not selected for';
       }
       const data = Utils().resSuccess(detectedPersonScores, message);
       return this.emit(SUCCESS, detectedPersonScores);
-      // model link and keypoint link from detectedPerson
-      //let response = this.ThirdPartyApis.callScoresGeneration(detectedPerson); 
-      //return;
     } catch(error) {
       switch(error.message) {
       case 'ValidationError':
