97b9522d56251a3975987e0f9e097fc019cd4f07
Kristine Boquiren <kboquiren@stratpoint.com>
2020-01-16 13:01:36 +0800

    Add middleware for authorization of page.

---
+++ src/interfaces/http/middlewares/authorizeMiddleware.js
@@ -0,0 +1,30 @@
+const Status = require('http-status');
+const jwtDecode = require('jwt-decode');
+
+const authorizeMiddleware = (req, res, next) => {
+  if (!req.headers['authorization']) {
+    return res.status(Status.UNAUTHORIZED).json({
+      type: 'Unauthorized',
+      details: [{
+        message: 'Unauthorized'
+      }]
+    });
+  }
+
+  let curEpoch = Math.round(Date.now()/1000);
+  let tokenExp = jwtDecode(req.headers['authorization']).exp;
+
+  if(curEpoch > tokenExp){
+    return res.status(Status.UNAUTHORIZED).json({
+      type: 'Unauthorized',
+      details: [{
+        message: 'Unauthorized'
+      }]
+    });
+  }else{
+    next();
+  }
+  
+};
+
+module.exports = authorizeMiddleware;
