095a6bca10fee48022bc2f4fbfc9db354eb3d7e2
Kristine Boquiren <kboquiren@stratpoint.com>
2020-02-10 11:23:22 +0800

    Move parameters to env.yml

--- src/infra/services/ThirdPartyApis.js
+++ src/infra/services/ThirdPartyApis.js
@@ -1,22 +1,11 @@
 const axios = require('axios');
-// async function getDetectedPerson(apiLink, params){
-//   try {
-//     const response = await axios.post(apiLink, params);
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// getDetectedPerson('https://9fa0c81c.ngrok.io/detect', {"clip_id": 58, "start":5, "video_path":"https://tennis-trainer-dev-media.s3.ap-southeast-1.amazonaws.com/videos/dev/2019-12-06/2019663293/Mandaluyong City_12062019_14:24.mp4"});
-//process.env.AI_ENGINE_LINK
 module.exports = class ThirdPartyApis {
   /* 
     Will generate detected persons from video
   */
   callPersonDetection(params){
-    params.debug = false;
-    return axios.post('http://ip-10-64-69-87.ap-southeast-1.compute.internal:6000/detect', params);
+    params.debug = process.env.AI_ENGINE_DEBUG;
+    return axios.post(`${process.env.AI_ENGINE_LINK}/detect`, params);
   }
 
   /* 
@@ -24,17 +13,17 @@ module.exports = class ThirdPartyApis {
   */
   callKeypointsExtraction(params){
     params.train_directly = false;
-    params.debug = false;
-    return axios.post('http://ip-10-64-69-87.ap-southeast-1.compute.internal:6000/extract', params);
+    params.debug = process.env.AI_ENGINE_DEBUG;
+    return axios.post(`${process.env.AI_ENGINE_LINK}/extract`, params);
   }
 
   callScoresGeneration(params){
-    params.debug = false;
-    return axios.post('http://ip-10-64-69-87.ap-southeast-1.compute.internal:6000/predict', params);
+    params.debug = process.env.AI_ENGINE_DEBUG;
+    return axios.post(`${process.env.AI_ENGINE_LINK}/predict`, params);
   }
 
   callModelTraining(params){
-    params.debug = false;
-    return axios.post('http://ip-10-64-69-87.ap-southeast-1.compute.internal:6000/train', params);
+    params.debug = process.env.AI_ENGINE_DEBUG;
+    return axios.post(`${process.env.AI_ENGINE_LINK}/train`, params);
   }
 };
