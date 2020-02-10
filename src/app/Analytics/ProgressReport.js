5946a841162e46b88fd37d1f28172f26ce112749
Kristine Boquiren <kboquiren@stratpoint.com>
2020-02-10 11:22:56 +0800

    Remove commented codes.

--- src/app/Analytics/ProgressReport.js
+++ src/app/Analytics/ProgressReport.js
@@ -22,39 +22,6 @@ class ProgressReport extends Operation {
     }
     try {
       const matches = await this.AnalyticsRepository.getProgressReports(params);
-      //   const matches = {
-      //     consistency:{
-      //       label : ['Match #1', 'Match #2', 'Match #3', 'Match #4', 'Match #5'],
-      //       data: [
-      //         {
-      //           name: '1st serve pts won',
-      //           scores: [80, 0, 78, 66, 80]
-      //         },
-      //         {
-      //           name: '2nd serve pts won',
-      //           scores: [72, 77, 70, 77, 72]
-      //         }
-      //         ,
-      //         {
-      //           name: 'Return pts won',
-      //           scores: [0, 77, 0, 77, 0]
-      //         }
-      //       ]
-      //     },
-      //     aggressiveness:{
-      //       label: ['Match #1', 'Match #2', 'Match #3', 'Match #4', 'Match #5'],
-      //       data: [
-      //         {
-      //           name: 'Aces/Double Faults',
-      //           scores: [12, 0, 0, 0, 0]
-      //         },
-      //         {
-      //           name: 'Winners/ Unforced Errors',
-      //           scores: [17, 20, 13, 30, 25]
-      //         }
-      //       ]
-      //     }
-      //   };
       return this.emit(SUCCESS, matches);
     } catch(error) {
       return this.emit(ERROR, error);
