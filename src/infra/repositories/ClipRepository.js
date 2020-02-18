e1e51a90168e07751da800694b748b2c0a53c534
Kristine Boquiren <kboquiren@stratpoint.com>
2020-02-18 16:05:03 +0800

    Use default getAll and move query to app layer

--- src/infra/repositories/ClipRepository.js
+++ src/infra/repositories/ClipRepository.js
@@ -105,43 +105,6 @@ class ClipRepository extends BaseRepository {
     return clipData;
   }
 
-  async getClipAnalytics(id, set) {
-    return this.model.findAll({
-      where: {
-        videoId: id,
-        set: set
-      },
-      attributes: [
-        'set',
-        'game',
-        'shotType',
-        'hitSpot',
-        'shotResult',
-        'smartPattern',
-        'extra',
-        'errorType',
-        'spin',
-        'shotDirection',
-        'speed'
-      ]
-    });
-  }
-
-  async getClips(id) {
-    return this.model.findAll({
-      where: {
-        videoId: id
-      },
-      attributes: {
-        exclude: [
-          'createdAt',
-          'updatedAt'
-        ]
-      }
-    });
-  }
-
-
   async getClipParent(id) {
     return await this._getById(id, {
       include: [
