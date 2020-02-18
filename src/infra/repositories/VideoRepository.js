673b4bd074ba1cbefb64bddeba7867cdf9a6f803
Kristine Boquiren <kboquiren@stratpoint.com>
2020-02-18 16:42:33 +0800

    Moved function to correct repository

--- src/infra/repositories/VideoRepository.js
+++ src/infra/repositories/VideoRepository.js
@@ -29,7 +29,57 @@ class VideoRepository extends BaseRepository {
       ]
     });
   }
+
+  getByFilter(data) {
+    var whereStatement = {};
+    if (data.videoId) {
+      whereStatement.videoId = data.videoId;
+    }
+    if (data.set) {
+      whereStatement.set = data.set;
+    }
+    if (data.game) {
+      whereStatement.game = data.game;
+    }
+    if (data.shotType) {
+      whereStatement.shotType = data.shotType;
+    }
+    if (data.hitSpot) {
+      whereStatement.hitSpot = data.hitSpot;
+    }
+    if (data.shotResult) {
+      whereStatement.shotResult = data.shotResult;
+    }
+    if (data.errorType) {
+      whereStatement.errorType = data.errorType;
+    }
+    if (data.shotDirection) {
+      whereStatement.shotDirection = data.shotDirection;
+    }
+    return this.model.findAll({
+      include: [{
+        model: this.ClipModel,
+        as: 'clips',
+        where: whereStatement,
+        attributes: {
+          exclude: [
+            'createdAt',
+            'updatedAt'
+          ],
+        }
+      }],
+      where: {
+        id: data.videoId
+      },
+      attributes: [
+        'videoName',
+        'matchType',
+        'location'
+      ]
+    });
+  }
 }
+
 
 module.exports = VideoRepository;
 
