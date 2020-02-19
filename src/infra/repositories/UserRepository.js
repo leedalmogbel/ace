74428c0aeb4a6867855a6d8c5a671d7fbf2a523c
Kristine Boquiren <kboquiren@stratpoint.com>
2020-02-19 17:58:57 +0800

    Remove unused function

--- src/infra/repositories/UserRepository.js
+++ src/infra/repositories/UserRepository.js
@@ -31,40 +31,6 @@ class UserRepository extends BaseRepository {
 
   }
 
-  async findCreateUpdate (data) {
-    const year = new Date().getFullYear();
-    const userId = parseInt(`${year}${Math.floor(Math.random()* 999999) + 100000}`);
-    return this.model.findOne({
-      where: {
-        email: data.email,
-      }
-    })
-      .then( (foundUser) => {
-        if (!foundUser) {
-          return this.model.create({
-            id: userId,
-            name: data.name,
-            email: data.email,
-            userType: data.userType,
-            googleUserId: data.googleUserId,
-            fbUserId: data.fbUserId,
-            subscribed: data.subscribed,
-          });
-        }
-        return this.model.update({
-          name: data.name,
-          userType: data.userType,
-          googleUserId: data.googleUserId,
-          fbUserId: data.fbUserId,
-          subscribed: data.subscribed,
-        }, {
-          where: { email: data.email },
-          returning: true,
-        // plain: true,
-        });
-      });
-  }
-
   async subscribed(id, data) {
     return this.model.upsert({
       id: id,
