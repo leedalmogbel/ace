6218fbc6b0b01f53b2eb52df8b3690153724edd9
Kristine Boquiren <kboquiren@stratpoint.com>
2020-03-18 16:34:59 +0800

    Update filter to accept array since its a checkbox

--- src/app/Clip/ClipFilter.js
+++ src/app/Clip/ClipFilter.js
@@ -1,4 +1,7 @@
 const { Operation } = require('@amberjs/core');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
 class ClipFilter extends Operation {
   constructor({ ClipRepository }) {
     super();
@@ -7,9 +10,40 @@ class ClipFilter extends Operation {
 
   async execute(data) {
     const { SUCCESS, NOT_FOUND } = this.events;
+    console.log(data);
+    let filters = {
+      videoId : data.videoId
+    };
+
+    if(data.shotType){
+      filters.shotType = {
+        [Op.in] : JSON.parse(data.shotType)
+      };
+    }
+
+    if(data.hitSpot){
+      filters.hitSpot = {
+        [Op.in] : JSON.parse(data.hitSpot)
+      };
+    }
+
+    if(data.errorType){
+      filters.errorType = {
+        [Op.in] : JSON.parse(data.errorType)
+      };
+    }
+
+    if(data.shotDirection){
+      filters.shotDirection = {
+        [Op.in] : JSON.parse(data.shotDirection)
+      };
+    }
+
+    console.log('FILTERS ', filters);
+
     try {
       const filtered = await this.ClipRepository.getAll({
-        where: data,
+        where: filters,
         attributes: {
           exclude: [
             'createdAt',
