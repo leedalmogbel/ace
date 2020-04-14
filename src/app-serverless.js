88e426810de468000dfc27a5dd5f6e6e1f751301
Kristine Boquiren <kboquiren@stratpoint.com>
2020-04-14 15:50:06 +0800

    Add Analytics Service to serverless

--- src/app-serverless.js
+++ src/app-serverless.js
@@ -3,12 +3,16 @@ const { Lifetime, asClass} = require('awilix');
 const { brew } = require('@amberjs/core');
 const config = require('config');
 const ThirdPartyApis = require('src/infra/services/ThirdPartyApis');
+const AnalyticsService = require('src/infra/services/AnalyticsService');
 
 module.exports.handler = (event, context, callback) => {
   brew(config, async (err, brewed) => {
     if (err) throw err;
     brewed.container.register({
       ThirdPartyApis: asClass(ThirdPartyApis, {
+        lifetime: Lifetime.SINGLETON
+      }),
+      AnalyticsService: asClass(AnalyticsService, {
         lifetime: Lifetime.SINGLETON
       })
     });
