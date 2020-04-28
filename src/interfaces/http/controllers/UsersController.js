f80e8a43d64bc3c535240401dba07b9ad3ff54b8
Kristine Boquiren <kboquiren@stratpoint.com>
2020-04-28 18:59:58 +0800

    Delete API of keypoints,models and scores per user

--- src/interfaces/http/controllers/UsersController.js
+++ src/interfaces/http/controllers/UsersController.js
@@ -16,6 +16,11 @@ class UsersController extends BaseController {
     router.put('/:id', this.injector('UpdateUser'), this.update);
     router.delete('/:id', this.injector('DeleteUser'), this.delete);
     // router.get('/auth-token', requestToken);
+
+    // Users keypoints,models and scores
+    router.delete('/:id/keypoints', this.injector('DeleteUserKeypoints'), this.delete);
+    router.delete('/:id/scores', this.injector('DeleteUserScores'), this.delete);
+    router.delete('/:id/models', this.injector('DeleteUserModels'), this.delete);
     
     //Coach
     router.get('/coach/list', this.injector('ListCoaches'), this.index);
@@ -67,6 +72,31 @@ class UsersController extends BaseController {
       .on(ERROR, next);
 
     operation.execute(req.body);
+  }
+
+  delete(req, res, next) {
+    const { operation } = req;
+    const { SUCCESS, ERROR,  NOT_FOUND, VALIDATION_ERROR } = operation.events;
+
+    operation
+      .on(SUCCESS, () => {
+        res.status(Status.ACCEPTED).end();
+      })
+      .on(NOT_FOUND, (error) => {
+        res.status(Status.NOT_FOUND).json({
+          type: 'NotFoundError',
+          details: error.details
+        });
+      })
+      .on(VALIDATION_ERROR, error => {
+        res.status(Status.BAD_REQUEST).json({
+          type: 'ValidationError',
+          details: error.details
+        });
+      })
+      .on(ERROR, next);
+
+    operation.execute(Number(req.params.id));
   }
 }
 
