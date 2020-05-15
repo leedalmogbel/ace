6e9453bcd583f7185954b693e6be55bed7ba374c
Kristine Boquiren <kboquiren@stratpoint.com>
2020-05-15 17:12:52 +0800

    Update authorizeMiddle ware to handle new id_token

--- src/interfaces/http/middlewares/authorizeMiddleware.js
+++ src/interfaces/http/middlewares/authorizeMiddleware.js
@@ -1,5 +1,6 @@
 const Status = require('http-status');
 const jwtDecode = require('jwt-decode');
+const { asValue } = require('awilix');
 
 const authorizeMiddleware = (req, res, next) => {
   if (!req.headers['authorization']) {
@@ -10,20 +11,62 @@ const authorizeMiddleware = (req, res, next) => {
       }]
     });
   }
-
+  
+  let decodedAuth = jwtDecode(req.headers['authorization']);
   let curEpoch = Math.round(Date.now()/1000);
-  let tokenExp = jwtDecode(req.headers['authorization']).exp;
+  let tokenExp = decodedAuth.exp;
+  let cognitoAppId = decodedAuth.aud;
 
-  if(curEpoch > tokenExp){
+  if(curEpoch > tokenExp || cognitoAppId != process.env.COGNITO_CLIENT_ID){
     return res.status(Status.UNAUTHORIZED).json({
       type: 'Unauthorized',
       details: [{
-        message: 'Unauthorized'
+        message: 'Invalid token.'
       }]
     });
-  }else{
-    next();
   }
+
+  let params = {
+    email : decodedAuth.email,
+    name : decodedAuth.name,
+    providerId : decodedAuth.identities[0].userId,
+    provider: decodedAuth.identities[0].providerName,
+  };
+
+
+  const authorize = req.container.resolve('Register');
+  const { SUCCESS, ERROR, VALIDATION_ERROR } = authorize.events;
+
+  authorize
+    .on(SUCCESS, (result) => {
+      req.container.register({ 
+        sessionUser: asValue(result)
+      });
+      next();
+    })
+    .on(VALIDATION_ERROR, (error) => {
+      res.status(Status.BAD_REQUEST).json({
+        type: 'ValidationError',
+        details: error.details
+      });
+    })
+    .on(ERROR, next);
+
+  authorize.execute(params);
+
+  // const register = req.container.resolve('Register');
+  // const { SUCCESS, ERROR } = authorize.events;
+
+  // register
+  //   .on(SUCCESS, (result) => {
+  //     req.container.register({ 
+  //       sessionUser: asValue(result)
+  //     });
+  //     next();
+  //   })
+  //   .on(ERROR, next);
+
+  // register.execute(email);
    
 };
 
