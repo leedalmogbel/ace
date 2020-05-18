c6d145ecd8ecf8e7113d92aedf2c46c4ea0a5d12
Kristine Boquiren <kboquiren@stratpoint.com>
2020-05-18 22:27:10 +0800

    Comment auto create db

--- src/infra/dataSources/Postgresqldb.js
+++ src/infra/dataSources/Postgresqldb.js
@@ -9,7 +9,7 @@ const config = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       dialect: process.env.DB_DIALECT,
-      isSync: 'true',
+      //isSync: 'true',
       //alter: true,
       logging: false,
       pool: {
@@ -29,7 +29,7 @@ const config = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       dialect: process.env.DB_DIALECT,
-     // isSync: 'true',
+      // isSync: 'true',
       logging: false,
       pool: {
         max: 50,
