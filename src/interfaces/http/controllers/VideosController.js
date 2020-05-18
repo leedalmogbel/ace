6ea7e32e651c7d00c4fca9f2f4366b2984450a87
Kristine Boquiren <kboquiren@stratpoint.com>
2020-05-19 00:01:51 +0800

    Remove unnecessary spaces

--- src/interfaces/http/controllers/VideosController.js
+++ src/interfaces/http/controllers/VideosController.js
@@ -9,12 +9,9 @@ class VideosController extends BaseController {
     const router = Router();
     // Video
     router.get('/:userId/video', this.injector('ListVideo'), this.showList);
-
     router.put('/:id/upload-status', this.injector('UpdateUploadStatus'), this.update);
     router.put('/:id/upload', this.injector('UpdateStatus'), this.update);
-
     //router.get('/analytics', this.injector('ListAnalytics'), this.showList);
-
     router.post('/getSignedURL', this.injector('GetSignedURL'), this.create);
 
     return router;
