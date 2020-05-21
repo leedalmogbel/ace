ae853e192abf75bb2d7be3cb70c63efa38a7bf31
Kristine Boquiren <kboquiren@stratpoint.com>
2020-05-21 19:27:03 +0800

    Alter old APIs. Add Show Clip

--- src/interfaces/http/controllers/ClipsController.js
+++ src/interfaces/http/controllers/ClipsController.js
@@ -10,8 +10,9 @@ class ClipsController extends BaseController {
     // Clip
     router.get('/', this.injector('ListClips'), this.showList);
     router.post('/:videoId/clip', this.injector('CreateClip'), this.create);
-    router.put('/update/:id', this.injector('UpdateClip'), this.update);
-    router.delete('/delete/:id', this.injector('DeleteClip'), this.delete);
+    router.get('/:id', this.injector('ShowClip'), this.show);
+    router.put('/:id', this.injector('UpdateClip'), this.update);
+    router.delete('/:id', this.injector('DeleteClip'), this.delete);
 
     router.get('/keypoint/list', this.injector('ListKeypoints'), this.index);
     router.get('/keypoint', this.injector('GetKeypoint'), this.showList);
